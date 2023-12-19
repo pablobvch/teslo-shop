e8a7967fdf811dedf0a761845534e7f2120a88f3
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2023-12-19 16:43:30 -0300

    fin clase 16

---
+++ src/seed/seedDatabase.ts
@@ -0,0 +1,48 @@
+import { initialData } from "./seed";
+import prisma from "../lib/prisma";
+
+async function main() {
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  //generar categories
+
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((category) => ({ name: category }));
+
+  await prisma.category.createMany({ data: categoriesData });
+
+  //generar products
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  //generar productImages
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+    const dbProduct = await prisma.product.create({
+      data: { ...rest, categoryId: categoriesMap[type] }
+    });
+
+    const imageData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id
+    }));
+
+    await prisma.productImage.createMany({ data: imageData });
+  });
+
+  console.log("Seed ejecutado correctamente");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+
+  main();
+})();
